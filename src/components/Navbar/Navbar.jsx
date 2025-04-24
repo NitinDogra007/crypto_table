acfc5d28688a4938a5b9b2ba1cfe00c25c873b72
Nitin Dogra <90884243+NitinDogra007@users.noreply.github.com>
2025-04-24 13:05:23 +1000

    fix nav bar links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,55 +1,53 @@
-import React, { useContext } from "react";
-import logo from "../../assets/logo.png";
-import { CoinContext } from "../../context/CoinContext";
-import "./Navbar.css";
-import { Link } from "react-router-dom";
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import { CoinContext } from '../../context/CoinContext';
+import './Navbar.css';
 
 function Navbar() {
-  const { setCurrency } = useContext(CoinContext);
+	const { setCurrency } = useContext(CoinContext);
 
-  function currencyHandler(event) {
-    switch (event.target.value) {
-      case "usd": {
-        setCurrency({ name: "usd", symbol: "$" });
-        break;
-      }
-      case "eur": {
-        setCurrency({ name: "eur", symbol: "€" });
-        break;
-      }
-      case "inr": {
-        setCurrency({ name: "inr", symbol: "₹" });
-        break;
-      }
-      default: {
-        setCurrency({ name: "usd", symbol: "$" });
-        break;
-      }
-    }
-  }
+	function currencyHandler(event) {
+		switch (event.target.value) {
+			case 'usd': {
+				setCurrency({ name: 'usd', symbol: '$' });
+				break;
+			}
+			case 'eur': {
+				setCurrency({ name: 'eur', symbol: '€' });
+				break;
+			}
+			case 'inr': {
+				setCurrency({ name: 'inr', symbol: '₹' });
+				break;
+			}
+			case 'aud': {
+				setCurrency({ name: 'aud', symbol: '$' });
+				break;
+			}
+			default: {
+				setCurrency({ name: 'usd', symbol: '$' });
+				break;
+			}
+		}
+	}
 
-  return (
-    <div className="navbar">
-      <Link to={"/"}>
-        <img src={logo} alt="logo" className="logo" />
-      </Link>
-      <ul>
-        <Link to={"/"}>
-          <li>Home</li>
-        </Link>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>Blog</li>
-      </ul>
-      <div className="nav-right">
-        <select onChange={currencyHandler}>
-          <option value="usd">USD</option>
-          <option value="eur">EUR</option>
-          <option value="inr">INR</option>
-        </select>
-      </div>
-    </div>
-  );
+	return (
+		<div className="navbar">
+			<Link to={'/'}>
+				<img src={logo} alt="logo" className="logo" />
+			</Link>
+			<ul></ul>
+			<div className="nav-right">
+				<select onChange={currencyHandler}>
+					<option value="usd">USD</option>
+					<option value="aud">AUD</option>
+					<option value="eur">EUR</option>
+					<option value="inr">INR</option>
+				</select>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;
