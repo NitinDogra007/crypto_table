c7bb42658d240dc160daa081cf43683b847c34b6
Nitin Dogra <90884243+NitinDogra007@users.noreply.github.com>
2025-04-25 14:47:15 +1000

    fix exposed key

--- src/context/CoinContextProvider.jsx
+++ src/context/CoinContextProvider.jsx
@@ -1,49 +1,49 @@
-import { useEffect, useState } from "react";
-import { CoinContext } from "./CoinContext";
+import { useEffect, useState } from 'react';
+import { CoinContext } from './CoinContext';
 
 function CoinContextProvider(props) {
-  const [allCoin, setAllCoin] = useState([]);
-  const [currency, setCurrency] = useState({
-    name: "usd",
-    symbol: "$",
-  });
+	const [allCoin, setAllCoin] = useState([]);
+	const [currency, setCurrency] = useState({
+		name: 'usd',
+		symbol: '$',
+	});
 
-  useEffect(() => {
-    async function fetchAllCoin() {
-      const options = {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          "x-cg-demo-api-key": "CG-gwkjwPGZdcobSjEicRN72mjj",
-        },
-      };
+	useEffect(() => {
+		async function fetchAllCoin() {
+			const options = {
+				method: 'GET',
+				headers: {
+					accept: 'application/json',
+					'x-cg-demo-api-key': import.meta.env.VITE_API_KEY,
+				},
+			};
 
-      try {
-        const res = await fetch(
-          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`,
-          options
-        );
-        const data = await res.json();
-        setAllCoin(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
+			try {
+				const res = await fetch(
+					`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`,
+					options
+				);
+				const data = await res.json();
+				setAllCoin(data);
+			} catch (error) {
+				console.error(error);
+			}
+		}
 
-    fetchAllCoin();
-  }, [currency]);
+		fetchAllCoin();
+	}, [currency]);
 
-  const contextValue = {
-    allCoin,
-    currency,
-    setCurrency,
-  };
+	const contextValue = {
+		allCoin,
+		currency,
+		setCurrency,
+	};
 
-  return (
-    <CoinContext.Provider value={contextValue}>
-      {props.children}
-    </CoinContext.Provider>
-  );
+	return (
+		<CoinContext.Provider value={contextValue}>
+			{props.children}
+		</CoinContext.Provider>
+	);
 }
 
 export default CoinContextProvider;
